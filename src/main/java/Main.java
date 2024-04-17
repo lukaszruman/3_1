be98218021d5be23eaaf1b29af9f1d9f2aeb988c
lukaszruman <161606263+lukaszruman@users.noreply.github.com>
2024-04-17 12:33:03 +0000

    3_1 prawidłowe

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,29 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        
         Scanner scanner = new Scanner(System.in);
-       
-        System.out.println("podaj pierwszy znak: ");
+
+        System.out.println("podaj liczbe wierszy: ");
+        int wiersz = scanner.nextInt();
+
+        System.out.println("podaj liczbe kolumn: ");
+        int kolumna = scanner.nextInt();
+        System.out.println("podaj pierwszy znak:" );
         char char1 = scanner.next().charAt(0);
+
         System.out.println("podaj drugi znak: ");
         char char2 = scanner.next().charAt(0);
-       
-        for (int i = 0; i < 6; i++) {
-            for (int j = 0; j < 6; j++) {
+        for (int i = 0; i < wiersz; i++) {
+            for (int j = 0; j < kolumna; j++) {
                 if ((i + j) % 2 == 0) {
                     System.out.print(char1 + " ");
                 } else {
                     System.out.print(char2 + " ");
                 }
             }
-            System.out.println(); 
+            System.out.println();
         }
-        
+
         scanner.close();
     }
 }
